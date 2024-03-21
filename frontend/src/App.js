d3ee0fcfb33273f66a1fe0bdfa6d5f322355e286
Atul Chandra <atulchandra021@gmail.com>
2024-03-21 21:43:36 +0530

    pushing frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage } from "./Routes.js";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/SignupPages.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Signup from "../components/Signup/Signup.jsx";
+const SignupPages = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default SignupPages;
