da3d3c06ea4ed3355e321fd271332d99617fd86b
Atul Chandra <atulchandra021@gmail.com>
2024-05-06 20:31:20 +0530

    beast deal featured products events part done

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import Header from "../components/Layout/Header.jsx";
+import Header from "../components/Layout/Header";
+import Hero from "../components/Route/Hero/Hero";
+import Categories from "../components/Route/Categories/Categories";
+import BestDeals from "../components/Route/BestDeals/BestDeals.jsx";
+import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct.jsx";
+import Events from "../components/Events/Events.jsx";
 
 const Homepage = () => {
-  return <div>
-   <Header activeHeading={1}/>
-  </div>;
+  return (
+    <div>
+      <Header activeHeading={1} />
+      <Hero />
+      <Categories/>
+      <BestDeals/>
+      <Events/>
+      <FeaturedProduct/>
+
+    </div>
+  );
 };
 
 export default Homepage;
